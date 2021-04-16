3451d212617e0c84c0002db96df6e386c33d09c8
Kacper Przybyszewski <kacper.przybyszewskiiii@gmail.com>
2021-04-16 14:20:36 +0200

    js code refactoring

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,27 @@
-let button = document.querySelector(".button");
-let body = document.querySelector(".body");
-
-button.addEventListener("click", () => {
-    body.classList.toggle("body--dark");
-})
-
-button.addEventListener("click", () => {
-    if (button.innerText === "Włącz ciemny motyw") {
-        button.innerText = "Wyłącz ciemny motyw";
-    } else {
-        button.innerText = "Włącz ciemny motyw";
+{
+    const welcome = () => {
+        console.log("ŚNIĄCY ZBUDŹ SIĘ!")
     }
-})
+    const toggleBackground = () => {
+        const body = document.querySelector(".body");
+        body.classList.toggle("body--dark");
+    }
+    const toggleButtonText = () => {
+        const button = document.querySelector(".button");
+        if (button.innerText === "Włącz ciemny motyw") {
+            button.innerText = "Wyłącz ciemny motyw";
+        } else {
+            button.innerText = "Włącz ciemny motyw";
+        }
+    }
+    const init = () => {
+        const button = document.querySelector(".button");
+
+        button.addEventListener("click", toggleBackground)
+        button.addEventListener("click", toggleButtonText)
+
+        welcome();
+
+    }
+    init();
+}
